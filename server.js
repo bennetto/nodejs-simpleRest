1190e3a9b95d268cf246afffd5aa691841b22a9d
bennetto <bbonnetto@gmail.com>
2014-10-21 23:19:00 +0200

    add witson logger

--- server.js
+++ server.js
@@ -7,6 +7,8 @@
 var express    = require('express'); 		// call express
 var app        = express(); 				// define our app using express
 var bodyParser = require('body-parser');
+var winston = require('winston');
+winston.add(winston.transports.File, { filename: 'simpleRest.log' });
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -28,8 +30,8 @@ var router = express.Router(); 				// get an instance of the express Router
 // middleware to use for all requests
 router.use(function(req, res, next) {
     // do logging
-    console.log('');
-    console.log('/* request begin */');
+    winston.info('');
+    winston.info('/* request begin */');
     next(); // make sure we go to the next routes and don't stop here
 });
 
@@ -43,7 +45,7 @@ router.route('/bears')
 
     // create a bear (accessed at POST http://localhost:8080/api/bears)
     .post(function(req, res) {
-        console.log('post bear');
+        winston.info('post bear');
         var bear = new Bear(); 		// create a new instance of the Bear model
         bear.name = req.body.name;  // set the bears name (comes from the request)
 
@@ -58,7 +60,7 @@ router.route('/bears')
     })
 // get all the bears (accessed at GET http://localhost:8080/api/bears)
     .get(function(req, res) {
-        console.log('get all bears');
+        winston.info('get all bears');
         Bear.find(function(err, bears) {
             if (err)
                 res.send(err);
@@ -73,8 +75,8 @@ router.route('/bears/:bear_id')
 
     // get the bear with that id (accessed at GET http://localhost:8080/api/bears/:bear_id)
     .get(function(req, res) {
-        console.log('get bear : ',req.params.bear_id);
-        console.log('bear_id');
+        winston.info('get bear : ',req.params.bear_id);
+        winston.info('bear_id');
         Bear.findById(req.params.bear_id, function(err, bear) {
             if (err)
                 res.send(err);
@@ -83,7 +85,7 @@ router.route('/bears/:bear_id')
     })
 // update the bear with this id (accessed at PUT http://localhost:8080/api/bears/:bear_id)
     .put(function(req, res) {
-        console.log('put bear : ',req.params.bear_id);
+        winston.info('put bear : ',req.params.bear_id);
         // use our bear model to find the bear we want
         Bear.findById(req.params.bear_id, function(err, bear) {
 
@@ -104,7 +106,7 @@ router.route('/bears/:bear_id')
     })
 // delete the bear with this id (accessed at DELETE http://localhost:8080/api/bears/:bear_id)
     .delete(function(req, res) {
-        console.log('delete bear : ',req.params.bear_id);
+        winston.info('delete bear : ',req.params.bear_id);
         Bear.remove({
             _id: req.params.bear_id
         }, function(err, bear) {
@@ -123,4 +125,4 @@ app.use('/api', router);
 // START THE SERVER
 // =============================================================================
 app.listen(port);
-console.log('Magic happens on port ' + port);
+winston.info('Magic happens on port ' + port);
