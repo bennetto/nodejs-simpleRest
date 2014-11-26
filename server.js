1dd0f1ac777a384ee85860bd4eabc72f15f45c1d
bennetto <bbonnetto@gmail.com>
2014-11-26 20:29:37 +0100

    begin webApp

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var express    = require('express'); 		// call express
 var app        = express(); 				// define our app using express
 var bodyParser = require('body-parser');
 var logger = require("./app/utils/logger.js");
+
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -25,6 +26,11 @@ var Apps = require('./app/models/Apps');
 
 logger.info("Require finished");
 global.app = app;
+
+
+logger.info("Public Folder : ",__dirname+ '/webSite');
+app.use("/public", express.static(__dirname+ '/webSite'));
+
 
 var _users = new Users('/users');
 
